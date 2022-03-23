847e780dc7fca397fa4dd29c0947aacdf0b41c9a
XuanRan_Nyan <31968665+xxxxuanran@users.noreply.github.com>
2022-03-23 16:33:50 +0800

    Add files via upload

---
+++ ffmpeg_install.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+LANG=en_US.UTF-8
+installPath=/usr/local/
+arch=$(uname -m)
+
+Check_Arch(){
+    if [ "$arch" == "aarch64" -o "$arch" == "arm64" ]; then
+        arch="arm64"
+    elif [ "$arch" == "x86_64" -o "$arch" == "amd64" ]; then
+        arch="amd64"
+    elif [ "$arch" == "i686" -o "$arch" == "i386" -o "$arch" == "i486" -o "$arch" == "i586" ]; then
+        arch="i686"
+    else
+        echo "Error: Not support OS Architecture!"
+        exit 1
+    fi
+}
+
+Download_FFMPEG(){
+    ln -sf bash /bin/sh
+    echo -e "This server's architecture is ${arch}\n"
+    fileName="ffmpeg-4.4.1-${arch}-static"
+    cd /tmp
+    wget -O ${fileName}.tar.xz https://www.johnvansickle.com/ffmpeg/old-releases/${fileName}.tar.xz
+    tar -xvf ${fileName}.tar.xz
+    rm -rf ${fileName}.tar.xz
+    mv ${fileName} /etc/ffmpeg
+    ln -s /etc/ffmpeg/ffmpeg /usr/local/bin/ffmpeg
+    ln -s /etc/ffmpeg/ffprobe /usr/local/bin/ffprobe
+    chmod a+x /etc/ffmpeg/*
+}
+
+Main(){
+    Check_Arch
+    Download_FFMPEG
+}
+
+Main
